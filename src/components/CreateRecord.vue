062bcf302e4bac930837072d9d7fd2e9058e9765
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2024-03-03 06:55:02 +0800

    update functionality for record creation

--- src/components/CreateRecord.vue
+++ src/components/CreateRecord.vue
@@ -1,12 +1,13 @@
 <template>
-  <Dialog :open="isModalVisible">
+  <Dialog v-model:open="isDialogOpen">
     <DialogTrigger as-child>
-      <Button @click="handleModalCreation"> Create Record </Button>
+      <Button> Create Record </Button>
     </DialogTrigger>
 
     <DialogContent class="sm:max-w-[40rem]">
       <DialogHeader>
         <DialogTitle> Create Record </DialogTitle>
+
         <DialogDescription>
           Complete answering the input fields then click create when you're done.
         </DialogDescription>
@@ -15,30 +16,21 @@
       <div class="grid gap-4 py-4">
         <div class="grid grid-cols-4 items-center gap-4">
           <Label for="boat-name" class="text-right"> Boat Name </Label>
-          <Input
-            v-model.trim="form.boatName!"
-            id="boat-name"
-            class="col-span-3"
-            placeholder="Ex. My Boat"
-            required
-          />
+          <Input v-model.trim="form.boatName!" id="boat-name" class="col-span-3" />
         </div>
+
         <div class="grid grid-cols-4 items-center gap-4">
           <Label for="serial-number" class="text-right"> Serial Number </Label>
-          <Input
-            v-model.trim="form.serialNumber!"
-            id="serial-number"
-            class="col-span-3"
-            placeholder="Ex. 123456789"
-            required
-          />
+          <Input v-model.trim="form.serialNumber!" id="serial-number" class="col-span-3" />
         </div>
+
         <div class="grid grid-cols-4 items-center gap-4">
           <Label for="destination" class="text-right"> Destination </Label>
-          <Select v-model="form.destination!" id="destination" required>
+          <Select v-model="form.destination!" id="destination">
             <SelectTrigger class="col-span-3">
-              <SelectValue placeholder="Select destination" />
+              <SelectValue />
             </SelectTrigger>
+
             <SelectContent>
               <SelectGroup>
                 <SelectLabel> Destinations </SelectLabel>
@@ -56,7 +48,29 @@
       </div>
 
       <DialogFooter>
-        <Button @click="handleRecordSaving" type="submit"> Create </Button>
+        <DialogClose as-child>
+          <Button type="button" variant="outline"> Cancel </Button>
+        </DialogClose>
+
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button type="button"> Done </Button>
+          </AlertDialogTrigger>
+
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle> {{ alertTitle }} </AlertDialogTitle>
+              <AlertDialogDescription> {{ alertDescription }} </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <AlertDialogFooter>
+              <AlertDialogCancel v-if="isFormFilledOut"> Cancel </AlertDialogCancel>
+              <AlertDialogAction @click="isFormFilledOut ? handleRecordSaving() : undefined">
+                {{ alertAction }}
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </DialogFooter>
     </DialogContent>
   </Dialog>
@@ -65,11 +79,11 @@
 <script lang="ts" setup>
 import destinations from "@/assets/data/destinations.json";
 import type { ICreateRecord, IRtdbResponse } from "@/assets/ts/interfaces";
-import { database } from "@/firebase/config";
 import { ref as useRtdbRef, onValue, set } from "firebase/database";
+import { database } from "@/firebase/config";
 import { useRecordStore } from "@/stores/record";
 import { useToast } from "@/components/ui/toast/use-toast";
-import { reactive, ref } from "vue";
+import { ref, reactive, computed, watch } from "vue";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -81,6 +95,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from "@/components/ui/dialog";
 import {
   Select,
@@ -91,11 +106,30 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const { toast } = useToast();
 const store = useRecordStore();
 const rtdbRef = useRtdbRef(database);
-const isModalVisible = ref(false);
+const isDialogOpen = ref(false);
+const isFormFilledOut = computed(() => !Object.values(form).some((value) => value === null));
+const alertTitle = computed(() => (isFormFilledOut.value ? "Saving Confirmation" : "Saving Error"));
+const alertDescription = computed(() =>
+  isFormFilledOut.value
+    ? "This action cannot be undone. This will create a new record as the currently active boat."
+    : "Please complete all the input fields to create a new record.",
+);
+const alertAction = computed(() => (isFormFilledOut.value ? "Create" : "Okay"));
 const rtdbData = reactive<IRtdbResponse>({
   IsAvailable: true,
   IsDeparted: false,
@@ -107,18 +141,6 @@ const form = reactive<ICreateRecord>({
   boatName: null,
   serialNumber: null,
   destination: null,
-});
-
-onValue(rtdbRef, (snapshot) => {
-  const data: any = snapshot.val();
-
-  if (data["0xb1"]) {
-    rtdbData.IsAvailable = data["0xb1"].IsAvailable;
-    rtdbData.IsDeparted = data["0xb1"].IsDeparted;
-    rtdbData.PassengerCount = data["0xb1"].PassengerCount;
-    rtdbData.IsLoading = data["0xb1"].IsLoading;
-    rtdbData.LoadingStatus = data["0xb1"].LoadingStatus;
-  }
 });
 
 function resetForm(): void {
@@ -137,29 +159,39 @@ async function updateDatabase(): Promise<void> {
   }
 }
 
-function handleModalCreation(): void {
-  resetForm();
-  isModalVisible.value = !isModalVisible.value;
-}
-
 async function handleRecordSaving(): Promise<void> {
   form.destination =
     destinations.destinations.find((destination) => destination.value === form.destination)
       ?.label ?? null;
 
-  if (!Object.values(form).some((value) => value?.trim().length === 0)) {
+  if (isFormFilledOut.value) {
     await updateDatabase().then(() => {
       store.createRecord(form);
 
       toast({
         title: "Record Successfully Created!",
-        description: `Boat: ${form.boatName} | Serial Number: ${form.serialNumber} | Destination: ${form.destination}`,
+        description: `BN: ${form.boatName} | SN: ${form.serialNumber} | D: ${form.destination}`,
         duration: 2500,
       });
 
-      resetForm();
-      isModalVisible.value = !isModalVisible.value;
+      isDialogOpen.value = false;
     });
   }
 }
+
+onValue(rtdbRef, (snapshot) => {
+  const data: any = snapshot.val();
+
+  if (data["0xb1"]) {
+    rtdbData.IsAvailable = data["0xb1"].IsAvailable;
+    rtdbData.IsDeparted = data["0xb1"].IsDeparted;
+    rtdbData.PassengerCount = data["0xb1"].PassengerCount;
+    rtdbData.IsLoading = data["0xb1"].IsLoading;
+    rtdbData.LoadingStatus = data["0xb1"].LoadingStatus;
+  }
+});
+
+watch(isDialogOpen, (newValue) => {
+  if (!newValue) resetForm();
+});
 </script>
