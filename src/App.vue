ba4058683d5b4e7d7ba1edc3c05e2bd40d749a63
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2024-03-11 20:35:36 +0800

    add table for history

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TheHeader />
 
     <main class="p-4">
-      <div v-if="store.createdAt !== 'N/A'" class="p-8 flex flex-col gap-4">
+      <div class="p-8 flex flex-col gap-4">
         <h1>{{ `Record for ${store.createdAt}` }}</h1>
 
         <div class="p-1">
@@ -53,7 +53,61 @@
         </div>
       </div>
 
-      <p v-else class="text-center">No active record</p>
+      <div class="px-8 pb-8">
+        <Button @click="useGetRecords">Refresh table</Button>
+
+        <div class="flex flex-col gap-4">
+          <div class="p-1">
+            <div
+              class="bg-primary text-primary-foreground rounded-t-lg grid grid-cols-7 text-center divide-x"
+            >
+              <p class="py-2 flex items-center justify-center">
+                Date <br />
+                and Time
+              </p>
+              <p class="py-2 flex items-center justify-center">Boat Name</p>
+              <p class="py-2 flex items-center justify-center">
+                Serial <br />
+                Number
+              </p>
+              <p class="py-2 flex items-center justify-center">Destination</p>
+              <p class="py-2 flex items-center justify-center">
+                Number of <br />
+                Passengers
+              </p>
+              <p class="py-2 flex items-center justify-center">Loading Status</p>
+              <p class="text-balance flex flex-col justify-center">
+                Departed <br />
+                Date and Time
+              </p>
+            </div>
+
+            <div
+              v-for="(record, index) in records"
+              :key="index"
+              class="bg-gray-100 text-gray-700 text-xs rounded-b-lg grid grid-cols-7 text-center divide-x"
+            >
+              <p class="py-2 text-pretty flex items-center justify-center">
+                {{ record.createdAt }}
+              </p>
+              <p class="py-2 text-pretty flex items-center justify-center">{{ record.boatName }}</p>
+              <p class="py-2 text-pretty flex items-center justify-center">
+                {{ record.serialNumber }}
+              </p>
+              <p class="py-2 text-pretty flex items-center justify-center">
+                {{ record.destination }}
+              </p>
+              <p class="py-2 text-pretty flex items-center justify-center">
+                {{ record.passengersCount }}
+              </p>
+              <p class="py-2 text-pretty flex items-center justify-center">
+                {{ record.loadingStatus }}
+              </p>
+              <p class="text-pretty flex flex-col justify-center">{{ record.departedAt }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
 
     <Toaster />
@@ -68,17 +122,29 @@
 import type { IResponse, IRtdbResponse } from "@/assets/ts/interfaces";
 import { ref as useRtdbRef, onValue, set } from "firebase/database";
 import { onAuthStateChanged } from "firebase/auth";
-import { database, auth } from "@/firebase/config";
+import { database, auth, db } from "@/firebase/config";
 import { useRecordStore } from "@/stores/record";
 import { ref, reactive } from "vue";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import Toaster from "@/components/ui/toast/Toaster.vue";
 import useSignIn from "@/firebase/auth/signin";
 import TheLoading from "@/components/TheLoading.vue";
 import TheHeader from "@/components/TheHeader.vue";
+import Button from "./components/ui/button/Button.vue";
+
+interface IRecordDB {
+  createdAt: string;
+  boatName: string;
+  serialNumber: string;
+  destination: string;
+  passengersCount: string;
+  loadingStatus: string;
+  departedAt: string;
+}
 
 const store = useRecordStore();
 const isAuthenticated = ref(false);
-
+const records = reactive<IRecordDB[]>([]);
 const rtdbData = reactive<IRtdbResponse>({
   IsAvailable: true,
   IsDeparted: false,
@@ -91,9 +157,11 @@ onAuthStateChanged(auth, async (user) => {
   if (user) {
     isAuthenticated.value = true;
 
+    await useGetRecords();
+
     const rtdbRef = useRtdbRef(database);
 
-    onValue(rtdbRef, (snapshot) => {
+    onValue(rtdbRef, async (snapshot) => {
       const data: any = snapshot.val();
 
       if (!data["0xb1"].IsAvailable && !data["0xb1"].IsDeparted && !data["0xb1"].IsLoading) {
@@ -110,6 +178,8 @@ onAuthStateChanged(auth, async (user) => {
         store.passengersCount = rtdbData.PassengerCount.toString();
         store.loadingStatus = rtdbData.LoadingStatus;
       } else {
+        store.setDeparted();
+
         set(rtdbRef, {
           "0xb1": {
             IsAvailable: false,
@@ -120,7 +190,19 @@ onAuthStateChanged(auth, async (user) => {
           },
         });
 
-        store.setDeparted();
+        try {
+          await addDoc(collection(db, "records"), {
+            createdAt: store.createdAt,
+            boatName: store.boatName,
+            serialNumber: store.serialNumber,
+            destination: store.destination,
+            passengersCount: store.passengersCount,
+            loadingStatus: store.loadingStatus,
+            departedAt: store.departedAt,
+          });
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
       }
     });
   } else {
@@ -136,4 +218,13 @@ onAuthStateChanged(auth, async (user) => {
     }
   }
 });
+
+async function useGetRecords(): Promise<void> {
+  const getRecords = await getDocs(collection(db, "records"));
+
+  getRecords.forEach((record) => {
+    const data = record.data();
+    records.push(data as IRecordDB);
+  });
+}
 </script>
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: IFirebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -18,5 +19,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const storage = getStorage(app);
+const db = getFirestore(app);
 
-export { auth, database, storage };
+export { auth, database, storage, db };
